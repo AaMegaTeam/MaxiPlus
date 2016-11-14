85b736dc9c5b633868e3655b750b7a020006b4a7
AaMegaTeam <hamed.dehghani321@gmail.com>
2016-11-14 20:16:40 +0330

    Update AboutMe.lua

--- plugins/AboutMe.lua
+++ plugins/AboutMe.lua
@@ -15,7 +15,7 @@ local function run(msg, matches)
   if matches[1] == 'bot' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "./data/me/version.webp", ok_cb, false)
-      return "<b>M A X I M U S +\n---------------------------------------------\n|<i>An Advanced Bot Based On LUA</i>|\n\n <b>Developer</b> : [@Teshne | @AminPmResan_Bot]\n---------------------------------------------\n> <b>Channel</b> : @pedaret\n\n<i>Thanks To</i><b>B L A C K +</b>"
+      return "<b>M A X I M U S +\n---------------------------------------------\n|<i>An Advanced Bot Based On LUA</i>|\n\n <b>Developer</b> : [@Teshne | @AminPmResan_Bot]\n---------------------------------------------\n> Channel : @pedaret"
     elseif is_admin1(msg) then
     send_document(get_receiver(msg), "./data/me/admin.webp", ok_cb, false)
       return "You're Admin"
